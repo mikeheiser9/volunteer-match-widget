bee1e6ecf3fee23095ba3ba70df61eaabc39511a
mikeheiser9 <mikeheiser9@gmail.com>
2020-06-04 17:17:56 -0400

    virtual location working w/ filters

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -8,11 +8,11 @@ import template from 'lodash/template';
 
 
 class Results extends React.Component {
-  state = { 
+  state = {
     query: gql`{
       searchOpportunities(
           input: {
-            location: "Sacremento, CA",
+            location: "Sacramento, CA",
             categories: [],
             virtual: false
           }
@@ -61,17 +61,18 @@ class Results extends React.Component {
       const object = this.props.form;
       let arr = [];
         for (const property in object) {
-         if (object[property] === true) {
+         if (object[property] === true && property !== 'virtual') {
           arr.push(property)
         }
       }
-      console.log(JSON.stringify(arr), object);
+      console.log(this.props.form);
       this.setState({
         query: gql`{
           searchOpportunities(
               input: {
                 location: "${this.props.form.location}",
-                categories: ${JSON.stringify(arr).replace(/["]+/g, '')} 
+                virtual: ${this.props.form.virtual},
+                categories: ${JSON.stringify(arr).replace(/["]+/g, '')}
               }) 
               {
               currentPage
@@ -121,16 +122,17 @@ class Results extends React.Component {
         {({ loading, error, data }) => {
           if (loading) return <p>Just a moment please...</p>
           if (error) return <p>Looks like we've got a problem... Plese try again</p>
-          console.log("this is where the data goes" + JSON.stringify(data.searchOpportunities.opportunities, null, 4));
+          // console.log("this is where the data goes" + JSON.stringify(data.searchOpportunities.opportunities, null, 4));
           return (
             <div className={"results-container"}>
              <div className={"city-header"}>
-              <h2>Opportunities Near {data.searchOpportunities.opportunities[0].location.city}</h2>
+              <h2>Opportunities Near {this.props.form.location 
+              || ('Sacramento')}</h2>
             </div>
             <div className={"results-inner"}>
              {data.searchOpportunities.opportunities.map((opportunities, index) => (
               <div className={"cards"} key={index}>
-               {/* { console.log(opportunities) } */}
+               { console.log(opportunities.location.virtual) }
                 <div className={"top-card"}>
                  <div className={"opp-img"}>
                   <div className={"img-inner"}>
@@ -140,12 +142,13 @@ class Results extends React.Component {
                 <div className={"org-date-cont"}>
                  <h3>{opportunities.parentOrg.name}</h3>
                   <span>Date: {opportunities.dateRange.startDate === null ? "Open Ended" : opportunities.dateRange.startDate}</span>
-               </div>
+               </div> 
               </div>
              <div className={"card-body"}>
               <div className={"category-cont"}>
                 { opportunities.categories.length === 3 ? 
                  <div className={"category-cont-inner"}>
+                  {/* <span>{opportunities.location.virtual === true ? ('Virtual') : ('not virtual')}</span> */}
                   <span>{startCase(camelCase(opportunities.categories[0]))}</span>
                   <span>{startCase(camelCase(opportunities.categories[1]))}</span>
                   <span>{startCase(camelCase(opportunities.categories[2]))}</span> 
